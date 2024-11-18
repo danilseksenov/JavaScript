8bc47543ec7a179cfc3df34e0b591d36fbd34207
danilseksenov <danil.seksenov@metropolia.fi>
2024-11-18 17:51:57 +0200

    update

---
+++ mod03/mod03/t1/1.js
@@ -0,0 +1,10 @@
+const ulElem = document.querySelector("#target")
+
+ulElem.innerHTML = 
+`
+<li>First item</li>
+<li>Second item</li>
+<li>Third item</li>
+`
+
+ulElem.classList.add("my-list")
---
+++ mod03/mod03/t2/2.js
@@ -0,0 +1,15 @@
+const target = document.getElementById("target")
+
+const item1 = document.createElement("li")
+item1.textContent = "First item"
+
+const item2 = document.createElement("li")
+item2.textContent = "Second item"
+item2.classList.add("my-item")
+
+const item3 = document.createElement("li")
+item3.textContent = "Third item"
+
+target.appendChild(item1)
+target.appendChild(item2)
+target.appendChild(item3)
